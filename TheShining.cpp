8c07072a4391044ca3661c423f91b56728eac7c6
renna3 <jrevrenna@gmail.com>
2024-12-12 11:10:48 +0100

    40punti sksk

--- TheShining.cpp
+++ TheShining.cpp
@@ -1,191 +1,128 @@
-
 #include <fstream>
 #include <vector>
-#include <set>
-#include <unordered_set>
-
+#include <iostream>
 
 using namespace std;
 
 int n_cicli = 0;
 vector<int> ciclo;
 
-class stack_polaretto{
+class StackPolaretto {
     private:
         vector<int> vec;
-        set<int> in_stack;
+        vector<bool> in_stack;
+
     public:
+        StackPolaretto(int n) : in_stack(n, false) {}
 
-    void push(int v){
-        vec.push_back(v);
-        in_stack.insert(v);
-    }
+        void push(int v) {
+            vec.push_back(v);
+            in_stack[v] = true;
+        }
 
-    void pop(){
-        int v = vec[vec.size() - 1];
-        vec.pop_back();
-        in_stack.erase(v);
-    }
+        void pop() {
+            int v = vec.back();
+            vec.pop_back();
+            in_stack[v] = false;
+        }
 
-    int top(){
-        return vec[vec.size() - 1];
-    }
+        int top() {
+            return vec.back();
+        }
 
-    bool isInStack(int n){
-        return in_stack.find(n) != in_stack.end(); // controlla
-    }
+        bool isInStack(int n) {
+            return in_stack[n];
+        }
 
-    bool isEmpty(){
-        return vec.size() == 0;
-    }
+        bool isEmpty() {
+            return vec.empty();
+        }
 
-
-    void getCycle(int n, vector<int> &conteggio){ // da chiamare solo quando si è sicuri, gestisco il caso in cui non ci sia ma è meglio non specare tempo
-        int i;
-        
-        if(n_cicli == 0){
-            for(i = vec.size() -1 ; i >= 0 && vec[i] != n ; --i){
+        void getCycle(int n, vector<int> &conteggio) {
+            ciclo.clear();
+            for (int i = vec.size() - 1; i >= 0; --i) {
                 ciclo.push_back(vec[i]);
                 ++conteggio[vec[i]];
+                if (vec[i] == n) break;
             }
-            conteggio[n]++;
-            ciclo.push_back(n);
-            ++n_cicli;
-        }else{
-            for(i = vec.size() -1 ; i >= 0 && vec[i] != n ; --i){
-                ++conteggio[vec[i]];
-            }
-            conteggio[n]++;
             ++n_cicli;
         }
-    }
 };
 
-class node{
-    bool swapped = false;
-    set<int> daVisitare;
-    set<int> visitati;
-    public:
-    set<int>& getVisistati(){
-        return swapped ? daVisitare : visitati;
-    }
-    set<int>& getDaVisitare(){
-        return swapped ? visitati : daVisitare;
-    }
+void trova_cicli(const vector<vector<int>> &grafo, vector<int> &conteggio, vector<bool> &visitati) {
+    int n = grafo.size();
+    StackPolaretto stack(n);
 
-    void swap(){
-        swapped = !swapped;
-    }
+    for (int start = 0; start < n; ++start) {
+        if (visitati[start]) continue;
 
-};
+        stack.push(start);
 
+        while (!stack.isEmpty()) {
+            int nodo = stack.top();
 
-void trova_cicli(vector<node> grafo, int num_nodi, vector<int>& conteggio, set<int>& nodi_visistare ){ // ritorna i cicli del grafo
-
-
-    stack_polaretto stack;
-
-  /*   vector<bool> visited(num_nodi,false); // inizializzo a false */
-
-    while(!nodi_visistare.empty()){
-
-        stack.push(*nodi_visistare.begin());
-
-        while(!stack.isEmpty()){
-
-            int elem = stack.top(); // prendo il primo elemento. (?) se zero NON è nel grafo?
-            nodi_visistare.erase(elem);
-
-           /*  visited[elem] = true; */
-
-            if(grafo[elem].getDaVisitare().empty()){ // se non ci sono elementi da visitare (non ci sono vicini)
-                grafo[elem].swap();
-                stack.pop();
-
-            }else{
-                // Prendo il primo elemento da `daVisitare`
-                auto it = grafo[elem].getDaVisitare().begin(); //restiutuisce un iteratore al primo elemento
-                int a = *it;
-
-                // Sposto l'elemento
-                grafo[elem].getVisistati().insert(a);
-                grafo[elem].getDaVisitare().erase(it);
-
-                if (stack.isInStack(a)) {
-                    // Trovato un ciclo, non pusho nello stack
-                    
-                    stack.getCycle(a, conteggio);
-
-                } else {
-                    // Aggiungi il nodo nello stack
-                    stack.push(a);
+            // Se tutti i vicini sono stati visitati o non ci sono vicini, esci
+            bool avanzato = false;
+            for (int vicino : grafo[nodo]) {
+                if (stack.isInStack(vicino)) {
+                    // Trovato un ciclo
+                    stack.getCycle(vicino, conteggio);
+                } else if (!visitati[vicino]) {
+                    // Continua il DFS
+                    stack.push(vicino);
+                    avanzato = true;
+                    break;
                 }
             }
 
+            if (!avanzato) {
+                stack.pop();
+                visitati[nodo] = true;
+            }
         }
-
     }
 }
 
+int main() {
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
 
-// quando faccio pop() devo guardare nel set 'daVisitare'
-
-
-int main(){
-
+    if (!inputFile) {
+        cerr << "Errore: impossibile aprire il file di input" << endl;
+        return -1;
+    }
 
     int numero_nodi, numero_archi;
-
-    ifstream inputFile("input.txt");
-    ofstream out("output.txt");
-
     inputFile >> numero_nodi >> numero_archi;
-    vector<node> grafo_iniziale(numero_nodi);
-    set<int> nodi_da_visitare;
-    for(int i = 0; i < numero_nodi; ++i){
-        nodi_da_visitare.insert(i);
+
+    // Lista di adiacenza per rappresentare il grafo
+    vector<vector<int>> grafo(numero_nodi);
+    for (int i = 0; i < numero_archi; ++i) {
+        int s, a;
+        inputFile >> s >> a;
+        grafo[s].push_back(a);
     }
-    int s,a;
-    for(int i = 0 ; i < numero_archi; ++i){
-        inputFile>>s>>a;
 
-        grafo_iniziale[s].getDaVisitare().insert(a);
-    }
-    //vector<set<int>> corridoi(numero_nodi);
+    // Inizializza il vettore per contare i nodi nei cicli e il bitmap per i nodi visitati
+    vector<int> conteggio(numero_nodi, 0);
+    vector<bool> visitati(numero_nodi, false);
 
-    vector<int> conteggio(numero_nodi,0);
+    // Trova cicli
+    trova_cicli(grafo, conteggio, visitati);
 
-    trova_cicli(grafo_iniziale,numero_nodi,conteggio,nodi_da_visitare);
-
-    /*
-    for(auto a: cicli){
-        for(auto b: a){
-            cout << b << " | ";
-        }
-        cout <<endl;
-    }*/
-
-
-    //cout << "cicli size: " << cicli.size() << endl;
-    for(int i = 0; i < conteggio.size(); ++i){
-        if(n_cicli == conteggio[i]) {
-            //cout << conteggio[i] << " ";
-            //cout << i << "";
-            out << i << endl;
-            out << ciclo.size()<<" ";
-            for(auto a = ciclo.rbegin(); a != ciclo.rend(); a++){
-                out<<*a<<" ";
+    // Cerca il nodo con il numero di cicli uguale a `n_cicli` e stampa
+    for (int i = 0; i < conteggio.size(); ++i) {
+        if (n_cicli == conteggio[i]) {
+            outputFile << i << endl;
+            outputFile << ciclo.size() << " ";
+            for (auto it = ciclo.rbegin(); it != ciclo.rend(); ++it) {
+                outputFile << *it << " ";
             }
-            out.close();
+            outputFile << endl;
             return 0;
         }
     }
-    out << "-1";
-    out.close();
+
+    outputFile << "-1";
     return 0;
 }
-
-//memorizzazione del file input nella lista di adiacenza
-
-
-
